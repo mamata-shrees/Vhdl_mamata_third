764f0fb835974e81dd93c9c6096c8aa5f49774a1
mamata-shrees <mamatasiris24@gmail.com>
2025-02-16 00:27:04 +0545

    Add files via upload

---
+++ binary_adder/binary_adder.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int binaryToDecimal(int binary) {
+    int decimal = 0, base = 1;
+    while (binary > 0) {
+        decimal += (binary % 10) * base;
+        binary /= 10;
+        base *= 2;
+    }
+    return decimal;
+}
+
+int decimalToBinary(int decimal) {
+    int binary = 0, base = 1;
+    while (decimal > 0) {
+        binary += (decimal % 2) * base;
+        decimal /= 2;
+        base *= 10;
+    }
+    return binary;
+}
+
+int main() {
+    int bin1, bin2;
+    printf("Enter two binary numbers: ");
+    scanf("%d %d", &bin1, &bin2);
+
+    int num1 = binaryToDecimal(bin1);
+    int num2 = binaryToDecimal(bin2);
+    int sum = num1 + num2;
+
+    printf("Sum: %d\n", decimalToBinary(sum));
+    return 0;
+}
+
---
+++ binary_subtractor/binary_subtractor.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+int binaryToDecimal(int binary) {
+    int decimal = 0, base = 1;
+    while (binary > 0) {
+        decimal += (binary % 10) * base;
+        binary /= 10;
+        base *= 2;
+    }
+    return decimal;
+}
+
+int decimalToBinary(int decimal) {
+    int binary = 0, base = 1;
+    while (decimal > 0) {
+        binary += (decimal % 2) * base;
+        decimal /= 2;
+        base *= 10;
+    }
+    return binary;
+}
+
+int main() {
+    int bin1, bin2;
+    printf("Enter two binary numbers: ");
+    scanf("%d %d", &bin1, &bin2);
+
+    int num1 = binaryToDecimal(bin1);
+    int num2 = binaryToDecimal(bin2);
+    int diff = num1 - num2;
+
+    if (diff < 0) {
+        printf("Error: Negative result in binary is not supported.\n");
+    } else {
+        printf("Difference: %d\n", decimalToBinary(diff));
+    }
+
+    return 0;
+}
+
