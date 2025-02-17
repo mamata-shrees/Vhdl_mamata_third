a298b4222753e7635ec137074dc45f1353cc91fd
mamata-shrees <mamatasiris24@gmail.com>
2025-02-17 21:08:27 +0545

    Add files via upload

---
+++ bitwise_operations/and.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void binary(int n) {
+    for (int i = 31; i >= 0; i--) 
+        printf("%d", (n >> i) & 1);
+}
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("\nBinary of %d: ", a);
+    binary(a);
+    printf("\nBinary of %d: ", b);
+    binary(b);
+
+    int result = a & b;
+    printf("\nBitwise AND: ");
+    binary(result);
+    printf("\nDecimal result: %d\n", result);
+
+    return 0;
+}
+
